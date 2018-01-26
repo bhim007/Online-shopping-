7174c9fad9fdc937f1a9e247f6849ee480c1d27a
bhimsaw <bhimsaw768@gmail.com>
2018-01-26 13:17:17 +0530

    My Project setup is completed

---
+++ shoppingbackend/src/main/java/bhim/net/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package bhim.net.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
